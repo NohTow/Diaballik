c112bc411ae9d4e254782e64f4677a45d07f24c3
Antoine CHAFFIN <antoine.chaffin@insa-rennes.fr>
2018-12-16 18:14:55 +0100

    Ajout de fonctionalités pour pouvoir jouer(Vision des mouvements possibles, mouvement executé..) ;

--- diaballik-frontend/src/app/board-component/board-component.component.ts
+++ diaballik-frontend/src/app/board-component/board-component.component.ts
@@ -11,18 +11,47 @@ import { MyData } from '../mydata';
 export class BoardComponentComponent implements OnInit {
 
   title = 'Game Board :';
+  public dataGame: MyData;
   
   constructor(private http: HttpClient, private router: Router, private data: MyData) { 
-    
+    this.dataGame = data;
   }
 
   ngOnInit() {
   }
 
-  public leftClick(event: MouseEvent): void {
-    
-    this.http.put("game/newGamePvP/5/Antoine/Adrien/Random", {}, {}).subscribe(returnedData => console.log(returnedData));
+  public leftClick(x: number, y: number): void {
+    if(this.dataGame.storage === ""){
+      this.http.put("game/newGamePvP/5/Antoine/Adrien/Random", {}, {}).subscribe(returnedData => this.dataGame.storage = returnedData);
+    }
+    console.log(this.dataGame.storage)
+    if(!(this.dataGame.isInList(x,y))){
+      this.dataGame.actualCase = JSON.parse('{"x":'+x+',"y":'+y+'}');
+     // console.log(this.dataGame.actualCase);
+     //this.http.put("game/newGamePvP/5/Antoine/Adrien/Random", {}, {}).subscribe(returnedData => console.log(returnedData));
+       this.http.get("game/moovePlayable/"+x+"/"+y+"",{}).subscribe(returnedData =>{
+        console.log("game/moovePlayable/"+x+"/"+y+"");
+        console.log(returnedData);
+        this.dataGame.testlist = returnedData;
+        //console.log(this.dataGame.testlist);
+    });
+  }else{
+    this.http.put("game/moove/"+this.dataGame.actualCase.x+"/"+x+"/"+this.dataGame.actualCase.y+"/"+y,{}).subscribe(returnedData =>{
+    this.dataGame.storage = returnedData;
+    this.dataGame.actualCase = "";
+    this.data.testlist = "";
+    });
+  }
+    //this.dataGame.testlist = JSON.parse('{"list":[{"x":'+x+',"y":'+y+'},{"x":3,"y":7}]}');
+   // this.http.put("game/newGamePvP/5/Antoine/Asn/Random", {}, {}).subscribe(returnedData => {
+     // console.log(returnedData);
+     // this.dataGame.storage = returnedData;
+     // console.log(this.dataGame.storage.joueur2.name);
+    //console.log(this.dataGame.isInList(1,1));
+    //console.log(this.dataGame.isInList(7,5));
+   // this.http.put("game/newGamePvP/5/Antoine/Adrien/Random", {}, {}).subscribe(returnedData => console.log(returnedData));
       
+   // });
   }
 
 
--- diaballik-frontend/src/app/mydata.ts
+++ diaballik-frontend/src/app/mydata.ts
@@ -9,10 +9,25 @@ export class MyData {
     // The data to store
     // any: https://www.typescriptlang.org/docs/handbook/basic-types.html
     public storage: any;
+    public testlist: any;
+    public actualCase: any;
 
     public constructor() {
         // An example of how storage can be used to store json data.
         // The JSON class can both parse JSON from a string, a produce a string from a JSON object (stringify)
-        this.storage = JSON.parse('{"type":"Game","@id":1,"idGame":5,"board":{"type":"Board","@id":2,"plateau":[[{"type":"Pawn","@id":3,"x":0,"y":0,"hasBall":false,"color":"Yellow"},{"type":"Pawn","@id":4,"x":0,"y":1,"hasBall":false,"color":"Yellow"},{"type":"Pawn","@id":5,"x":0,"y":2,"hasBall":false,"color":"Yellow"},{"type":"Pawn","@id":6,"x":0,"y":3,"hasBall":true,"color":"Yellow"},{"type":"Pawn","@id":7,"x":0,"y":4,"hasBall":false,"color":"Yellow"},{"type":"Pawn","@id":8,"x":0,"y":5,"hasBall":false,"color":"Yellow"},{"type":"Pawn","@id":9,"x":0,"y":6,"hasBall":false,"color":"Yellow"}],[null,null,null,null,null,null,null],[null,null,null,null,null,null,null],[null,null,null,null,null,null,null],[null,null,null,null,null,null,null],[null,null,null,null,null,null,null],[{"type":"Pawn","@id":10,"x":6,"y":0,"hasBall":false,"color":"Green"},{"type":"Pawn","@id":11,"x":6,"y":1,"hasBall":false,"color":"Green"},{"type":"Pawn","@id":12,"x":6,"y":2,"hasBall":false,"color":"Green"},{"type":"Pawn","@id":13,"x":6,"y":3,"hasBall":true,"color":"Green"},{"type":"Pawn","@id":14,"x":6,"y":4,"hasBall":false,"color":"Green"},{"type":"Pawn","@id":15,"x":6,"y":5,"hasBall":false,"color":"Green"},{"type":"Pawn","@id":16,"x":6,"y":6,"hasBall":false,"color":"Green"}]],"list":[3,4,5,6,7,8,9,10,11,12,13,14,15,16]},"nbAction":0,"numTurn":1,"color":"Yellow","hasIA":false,"save":[],"undo":[],"gameBoard":2,"joueur1":{"type":"Humain","@id":17,"name":"Antoine","color":"Yellow"},"joueur2":{"type":"Humain","@id":18,"name":"Adrien","color":"Green"}}');
+        this.storage = ""
+        this.testlist = "";
+        this.actualCase = "";
     }
+
+    public isInList(x: number, y: number): boolean{
+        let res: boolean = false;
+        for(let elem of this.testlist){
+            if(elem.newX === x && elem.newY === y){
+                res = true;
+            }
+        }
+        return res;
+    }
+       
 }
